863f6d6738c23355ef5af38470d13623d3a240f5
Roshan Studley <OnyxAbove@users.noreply.github.com>
2024-10-25 18:45:06 -0700

    edit: 2024/10/25/1

--- init.lua
+++ init.lua
@@ -109,4 +109,5 @@ require 'lazy-plugins'
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
 
+-- [[ Colorscheme ]]
 vim.cmd.colorscheme 'catppuccin-macchiato'
