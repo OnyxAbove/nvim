53be51e1be2cdd55ab64b281b04429728ce6d78a
Roshan Studley <OnyxAbove@users.noreply.github.com>
2025-02-25 20:24:57 -0800

    edit: 2/25/2025-1

--- lua/pluginconf.lua
+++ lua/pluginconf.lua
@@ -1,3 +1,15 @@
 -- PLUGIN CONFIGURATION
 
-require("bufferline").setup{}
+require('bufferline').setup {}
+
+require('cmp').setup {
+  mapping = {
+    ['<CR>'] = function(fallback)
+      if require('cmp').visible() then
+        require('cmp').confirm()
+      else
+        fallback()
+      end
+    end,
+  },
+}
