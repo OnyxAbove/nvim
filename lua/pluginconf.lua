87681486272045d61cb06e9fd544de35d45bbd62
Roshan Studley <OnyxAbove@users.noreply.github.com>
2025-02-26 20:04:09 -0800

    edit: 2/26/2025-1

--- lua/pluginconf.lua
+++ lua/pluginconf.lua
@@ -13,3 +13,34 @@ require('cmp').setup {
     end,
   },
 }
+
+local wk = require 'which-key'
+
+wk.setup {
+  -- your existing configuration options
+}
+
+wk.add {
+  { '<leader>e', group = '[E]xplorer' },
+  { '<leader>ec', '<cmd>NvimTreeCollapse<CR>', desc = 'Collapse file explorer' },
+  { '<leader>ee', '<cmd>NvimTreeFocus<CR>', desc = 'Toggle file explorer' },
+  { '<leader>ef', '<cmd>NvimTreeFindFileToggle<CR>', desc = 'Toggle file explorer on current file' },
+  { '<leader>er', '<cmd>NvimTreeRefresh<CR>', desc = 'Refresh file explorer' },
+  { '<leader>o', group = '[O]pen' },
+  { '<leader>ot', '<cmd>terminal<CR>', desc = 'Open a terminal in a new buffer' },
+}
+
+--[[
+wk.register({
+  o = {
+    name = '[O]pen',
+    t = { '<cmd>ToggleFunction<CR>', 'Toggle Function' },
+  },
+  e = {
+    e = { '<cmd>NvimTreeFocus<CR>', 'Toggle file explorer' }, -- toggle file explorer
+    f = { '<cmd>NvimTreeFindFileToggle<CR>', 'Toggle file explorer on current file' }, -- toggle file explorer on current file
+    c = { '<cmd>NvimTreeCollapse<CR>', 'Collapse file explorer' }, -- collapse file explorer
+    r = { '<cmd>NvimTreeRefresh<CR>', 'Refresh file explorer' }, -- refresh file explorer
+  },
+}, { prefix = '<leader>' })
+]]
